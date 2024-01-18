105b8f87c9d8e5a2f8b6db3f321b9a03a64f44d1
shrikant <kamle.shrikant@gmail.com>
2024-01-18 16:55:23 +0530

    serveless testin with dummy api url

--- handler.py
+++ handler.py
@@ -1,7 +1,11 @@
 import json
+import requests
+
+base_url = "https://jsonplaceholder.typicode.com/"
 
 def hello(event,context):
-    body = {"message" : "my first test api with serverless",input: event}
+    resp = requests.get(url = base_url + "/todo/1")
+    # body = {"message" : "my first test api with serverless","input": event}
 
-    return {"status_code":200,"body": json.dumps(body)}
+    return {"status_code":resp.status_code,"body": resp.json()}
 
