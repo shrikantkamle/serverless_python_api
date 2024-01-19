79927e8995081e132f4dc8d4611731424efc9b97
shrikant <kamle.shrikant@gmail.com>
2024-01-19 18:21:34 +0530

    used urllib as requests is not supported abve 3.7 in aws

--- handler.py
+++ handler.py
@@ -1,11 +1,29 @@
 import json
-import requests
+# import requests
 
 base_url = "https://jsonplaceholder.typicode.com/"
 
 def hello(event,context):
-    resp = requests.get(url = base_url + "/todo/1")
-    # body = {"message" : "my first test api with serverless","input": event}
+    # resp = requests.get(url = base_url + "todo/1")
+    body = {"message" : "my first test api with serverless","input": event}
 
-    return {"status_code":resp.status_code,"body": resp.json()}
+    # return {"status_code":resp.status_code,"body": resp.json()}
+
+import json
+from urllib.request import urlopen
+from config import base_url
+
+def lambda_handler(event, context):
+    # TODO implement
+    url = "https://jsonplaceholder.typicode.com/todos/1"
+    with urlopen(url) as response:
+        body = response.read()
+        status = response.getcode()
+    
+    todo_item = json.loads(body)
+    
+    return {
+            'statusCode': status,
+            'body': todo_item
+        }
 
